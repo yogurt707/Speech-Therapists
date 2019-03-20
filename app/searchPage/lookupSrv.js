7fe41e78668b0a7d291b6baa93d4b5ade3ff7707
yogurt707 <akiva707@gmail.com>
2019-03-20 15:20:34 +0200

    lookupSrv + jsons

--- app/searchPage/lookupSrv.js
+++ app/searchPage/lookupSrv.js
@@ -1,25 +1,91 @@
-app.factory("lookupSrv", function () {
+app.factory("lookupSrv", function ($http, $q) {
 
 
-    function getRegions() {
-        return ["גליל עליון", "גליל תחתון", "אצבע הגליל", "בקעת הירדן", "הערבה", "הנגב", "השפלה", "השרון", "חיפה והסביבה", "יהודה", "השומרון", "השפלה", "מדבר יהודה", "עמק יזרעל",
-            "אילת והסביבה", "ירושלים והסביבה", "ים המלח והסביבה", "רמת הגולן", "הגליל המערבי", "מישור החוף הצפוני", "מישור החוף הדרומי", "גוש דן", "בקעת באר שבע", "מכתשים בנגב",
-            "הר הנגב", "הרי אילת", "חבל הבשור", "ארץ כינורות", "בקעת בית שאן", "בקעת ים המלח"];
+    function Clin(clinObject) {
+
+        this.fname = clinObject.fname;
+        this.lname = clinObject.lname;
+        this.image = clinObject.image;
+        this.therapyType= clinObject.therapyType;
+        this.hasClinic = clinObject.hasClinic;
+        this.id = clinObject.id;
+        this.isWillingToAriveHome = clinObject.isWillingToAriveHome;
+        this.email = clinObject.email;
+        this.phone = clinObject.phone;
+        this.availableDates = clinObject.availableDates;
+        this.city = clinObject.city;
+        this.region = clinObject.region;
+        this.ismale = clinObject.ismale;
     }
 
-    function getTreatments() {
 
-        return ["cp-סיפי", "אוטיזם-הפרעות-קשר-ילדים", "אוטיזם-הפרעות-קשר-מתבגרים", "אכילה-לעיסה-ובליעה-ילדים", "אכילה-לעיסה-ובליעה-מבוגרים", "בדיקות-שמיעה-והתאמת-מכשירי-שמיעה",
-            "גמגום-והפרעות-בשטף-הדיבור-מבוגרים", "גמגום-והפרעות-בשטף-הדיבור-מבוגרים", "גמגום-והפרעות-בשטף-הדיבור-ילדים", "דחיקת-לשון", "דיקציה", "דיספרקסיה-של-הדיבור",
-            "הפרעות-קול-ובליעה-על-רקע-נוירולוגי", "לקויות-שפה-אצל-ילדים", "לקויי-השפה-והדיבור-נרכשות-אפזיה-ילד", "לקויי-השפה-והדיבור-נרכשות-אפזיה-מבו",
-            "לקויי-למידה-וטיפולי-שפה-לילדי-בית-ספר", "צרידות-ולקויות-בקול-ילדים", "צרידות-ולקויות-בקול-מבוגרים", "קשיי-דיבור-על-רקע-מבני", "שיום-שמיעה", "תקשורת-תומכת-חליפית"];
+    //Brings all data from json (Regions data)
+    function getRegions() {
+        var async = $q.defer();
+        var Regions = [];
+
+        // Get All Regions 
+        $http.get("app/service/regions.json").then(function (res) {
+            // success
+            for (var i = 0; i < res.data.length; i++) {
+                Regions.push(res.data[i]);
+            }
+            async.resolve(Regions); // resolving the promise with the Regions array      
+        }, function (err) {
+            // error
+            console.error(err);
+            async.reject(err);  // rejecting the promise
+        });
+        return async.promise;
+    }
+
+
+    //Brings all data from json (Treatments data)
+    function getTreatments() {
+        var async = $q.defer();
+        var treatments = [];
+
+        // Get All treatments 
+        $http.get("app/service/treatments.json").then(function (res) {
+            // success
+            for (var i = 0; i < res.data.length; i++) {
+                treatments.push(res.data[i]);
+            }
+            async.resolve(treatments); // resolving the promise with the treatments array      
+        }, function (err) {
+            // error
+            console.error(err);
+            async.reject(err);  // rejecting the promise
+        });
+        return async.promise;
+    }
+    
+
+    //Brings all data from json (clinList data)
+    function getClinList() {
+        var async = $q.defer();
+        var clinList = [];
+
+        // Get All clinList 
+        $http.get("app/service/clinList.json").then(function (res) {
+            // success
+            for (var i = 0; i < res.data.length; i++) {
+                clinList.push(new Clin(res.data[i]));
+            }
+            async.resolve(clinList); // resolving the promise with the clinList array      
+        }, function (err) {
+            // error
+            console.error(err);
+            async.reject(err);  // rejecting the promise
+        });
+        return async.promise;
     }
 
 
     return {
         getRegions: getRegions,
-        getTreatments: getTreatments
+        getTreatments: getTreatments,
+        getClinList: getClinList
     }
 
-    });
-    
+});
--- app/searchPage/searchCtrl.js
+++ app/searchPage/searchCtrl.js
@@ -1,7 +1,22 @@
 app.controller("searchCtrl", function ($scope, lookupSrv) {
 
 
-    $scope.regions = lookupSrv.getRegions();
-    $scope.Treatments = lookupSrv.getTreatments();
+    //loads all data from SRV
+    
+    lookupSrv.getRegions().then(function(results){
+        $scope.regions = results;
+        // console.log(JSON.stringify($scope.regions))  //Checks if all data returned after the promiss
+    });
 
+    lookupSrv.getTreatments().then(function(results){
+        $scope.Treatments = results;
+        // console.log(JSON.stringify($scope.Treatments))  //Checks if all data returned after the promiss
+    });
+
+    lookupSrv.getClinList().then(function(results){
+        $scope.clinList = results;
+        // console.log(JSON.stringify($scope.clinList))  //Checks if all data returned after the promiss
+    });
+
+   
 });     
